83107f9c06c1e94637eff940491efd8004adf904
r_rank_pyramid <r_rank_pyramid@foxmail.com>
2023-04-20 18:18:47 +0800

    fix contract

--- website/src/constants/contracts.js
+++ website/src/constants/contracts.js
@@ -1,5 +1,8 @@
+// using `ganache-cli -p 7545 --deterministic` as a local blockchain
+// if you want to use another network, you need to change the address of the contract
+// remove deterministic if you want to use a different address and higher security.
+
 const addresses = {
-  NFT_CONTRACTS_ADDRESS: "0x4F0F330e4A9F064EaE6Ca44013198548B7bf80A8",
+  NFT_CONTRACTS_ADDRESS: "0xCfEB869F69431e42cdB54A4F4f105C19C080A601",
 };
 export default addresses;
-
